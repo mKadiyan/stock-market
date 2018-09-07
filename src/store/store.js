64fce789ea7051ff28278cd5acae8cd7ca8d6139
Ravinder Kadiyan <rkadiyan@thoughtworks.com>
2018-09-07 08:21:11 +0530

    initial setup

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import stocks from './modules/stocks'
+import portfolio from './modules/portfolia'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    stocks,
+    portfolio
+  }
+})
